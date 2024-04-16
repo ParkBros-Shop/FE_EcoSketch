2e2fe8cf67bd9c7883abc6bf76f116a56a8581fa
Luison1472 <melody9908@naver.com>
2024-04-16 13:05:49 +0900

    Footer 내용 수정

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -10,16 +10,18 @@ const Footer = () => {
           <li className="mr-1">상호명 :</li>
           <li className="mr-2.5">에코스케치</li>
           <li className="mr-2.5">|</li>
-          <li className="mr-2.5">사업자등록번호 : 000-00-00000</li>
+          <li className="mr-2.5">사업자등록번호 : 897-29-01364</li>
+          <li className="mr-2.5">|</li>
+          <li className="mr-2.5">통신판매업 신고 : 제2023-경기김포-3650 [링크]</li>
           <li className="mr-2.5">|</li>
           <li className="mr-2.5">대표자 : 김옥화</li>
         </ul>
         <ul className="flex">
-          <li className="mr-2.5">주소 : 인천ㅇㅇㅇㅇㅇ</li>
+          <li className="mr-2.5">주소 : 경기 김포시 양촌읍 황금산단로 57 지케이디프라자 1층</li>
           <li className="mr-2.5">|</li>
           <li className="mr-2.5">이메일 : kow0308@naver.com</li>
           <li className="mr-2.5">|</li>
-          <li className="mr-2.5">고객센터 : 010-0000-0000</li>
+          <li className="mr-2.5">고객센터 : 031-982-7793</li>
         </ul>
 
         <ul className="flex items-center align-middle">
