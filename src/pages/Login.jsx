ff3823ed957d2e17855af573427c26f139a54513
Luison1472 <melody9908@naver.com>
2024-05-31 18:42:47 +0900

    Login&SignUp CreateUI

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -4,54 +4,96 @@ import { Link } from 'react-router-dom';
 const Login = () => {
   return (
     <>
-      <div className="min-h-screen flex justify-center bg-white py-12 px-4 sm:px-6 lg:px-8">
-        <Link to="/">
-          <div className="absolute left-6 top-14 w-5">
-            <img src="/src/assets/left-arrow.png" alt="왼쪽 화살표"/> 
-          </div>
-        </Link>
-        <div className="max-w-md w-full space-y-8">
-          <div>
-            <h2 className="ml-5 mt-14 text-3xl flex item-start font-extrabold text-gray-900">
-              이메일로 로그인
-            </h2>
+      <div className="min-h-screen flex justify-center items-center bg-gray-100 py-12 px-4 sm:px-6 lg:px-8">
+        <div className="max-w-md w-full space-y-8 bg-white p-8 rounded-lg shadow-lg">
+          
+          <div className="text-center">
+            <h2 className="text-3xl font-extrabold text-gray-900">로그인</h2>
           </div>
           <form className="mt-8 space-y-6" action="#" method="POST">
-            <div className="rounded-full shadow-sm -space-y-px">
-              <div>
-                <p className="flex items-start text-blue-500 text-xs">이메일 주소</p>
-                <label htmlFor="email-address" className="sr-only">이메일 주소</label>
-                <input id="email-address" name="email" type="email" autoComplete="email" required className="text-xl mb-2 relative block w-full px-3 py-1 border-b-2 border-gray-600 bg-transparent text-black rounded-t-md focus:outline-none focus:ring-blue-500 focus:border-blue-500 focus:z-10 sm:text-sm" placeholder="이메일 주소"/>
+            <div className="rounded-md shadow-sm -space-y-px">
+              <div className="mb-4">
+                <label htmlFor="email-address" className="text-left pb-2 block text-sm font-medium text-gray-700">
+                  이메일 주소
+                </label>
+                <input
+                  id="email-address"
+                  name="email"
+                  type="email"
+                  autoComplete="email"
+                  required
+                  className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-blue-500 focus:border-blue-500 focus:z-10 sm:text-sm"
+                  placeholder="이메일 주소"
+                />
               </div>
-              <div>
-                <p className="mt-28 flex items-start text-blue-500 text-xs">비밀번호</p>
-                <label htmlFor="password" className="sr-only">비밀번호</label>
-                <input id="password" name="password" type="password" autoComplete="current-password" required className="text-xl mb-2 relative block w-full px-3 py-1 border-b-2 border-gray-600 bg-transparent text-black rounded-t-md focus:outline-none focus:ring-blue-500 focus:border-blue-500 focus:z-10 sm:text-sm" placeholder="비밀번호"/>
+              <div className="mb-6">
+                <label htmlFor="password" className=" text-left pb-2 block text-sm font-medium text-gray-700">
+                  비밀번호
+                </label>
+                <input
+                  id="password"
+                  name="password"
+                  type="password"
+                  autoComplete="current-password"
+                  required
+                  className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-blue-500 focus:border-blue-500 focus:z-10 sm:text-sm"
+                  placeholder="비밀번호"
+                />
               </div>
-            </div>    
-            <div className="flex items-center justify-between">
+            </div>
+            <div className="flex items-center justify-between mb-6">
               <div className="flex items-center">
-                <input id="remember_me" name="remember_me" type="checkbox" className="h-5 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded"/>
+                <input
+                  id="remember_me"
+                  name="remember_me"
+                  type="checkbox"
+                  className="h-4 w-4 text-green-600 focus:ring-blue-500 border-gray-300 rounded"
+                />
                 <label htmlFor="remember_me" className="ml-2 block text-sm text-gray-900">
-                  Remember me
+                  로그인 유지
                 </label>
               </div>
               <div className="text-sm">
-                <a href="#" className="font-medium text-blue-500 hover:text-indigo-500">
-                  Forgot your password?
-                </a>
+                <Link to="/member/changePwd" className="font-medium text-green-700 hover:text-green-500">
+                  비밀번호를 잊으셨나요?
+                </Link>
               </div>
             </div>
             <div>
-              <button type="submit" className="group relative w-full flex justify-center items-center h-14 py-2 px-4 border-none text-xl font-medium rounded-md text-white bg-gray-500 hover:bg-blue-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-700">
+              <button
+                type="submit"
+                className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
+              >
                 <span className="absolute left-0 inset-y-0 flex items-center pl-3">
-                  <svg className="h-5 w-5 text-black group-hover:text-blue-300" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                    <path fillRule="evenodd" d="M10 12a2 2 0 100-4 2 2 0 000 4z" clipRule="evenodd" />
-                    <path fillRule="evenodd" d="M4 8V6a4 4 0 118 0v2h1a1 1 0 011 1v7a1 1 0 01-1 1H4a1 1 0 01-1-1v-7a1 1 0 011-1h1zm4-3a2 2 0 10-4 0v2h4V5z" clipRule="evenodd" />
+                  <svg
+                    className="h-5 w-5 text-white "
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                    aria-hidden="true"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M10 12a2 2 0 100-4 2 2 0 000 4z"
+                      clipRule="evenodd"
+                    />
+                    <path
+                      fillRule="evenodd"
+                      d="M4 8V6a4 4 0 118 0v2h1a1 1 0 011 1v7a1 1 0 01-1 1H4a1 1 0 01-1-1v-7a1 1 0 011-1h1zm4-3a2 2 0 10-4 0v2h4V5z"
+                      clipRule="evenodd"
+                    />
                   </svg>
                 </span>
                 로그인
               </button>
+            </div>
+            <div className="text-center mt-4">
+              <p className="text-sm text-gray-600">
+                계정이 없으신가요?{' '}
+                <Link to="/member/signup" className="font-medium text-green-700 hover:text-green-500">
+                  회원가입
+                </Link>
+              </p>
             </div>
           </form>
         </div>
---
+++ src/pages/Product1kg.jsx
@@ -0,0 +1,37 @@
+import React, { useEffect, useState } from 'react';
+import { fetchBestProducts } from '/src/api/productAPI.js';
+import { Link } from 'react-router-dom';
+import { useQuery } from '@tanstack/react-query';
+
+const Product1kg = () => {
+  const { data, isLoading, error } = useQuery({
+    queryKey: ['bestProducts'],
+    queryFn: fetchBestProducts,
+  });
+
+  const formatPrice = (price) => {
+    return new Intl.NumberFormat('ko-KR').format(price) + '원';
+  };
+
+  if (isLoading) return <div>Loading...</div>;
+  if (error) return <div>Error: {error.message}</div>;
+
+  return (
+    <div className="flex flex-wrap justify-center">
+      {data.map(data => (
+      <Link to={`/product/${data.productId}`} key={data.productId}>
+        <div key={data.id} className="border border-gray-300 mt-40 rounded-lg p-4 m-4 w-64 text-center">
+          <img src={data.firstImage} alt={data.description} className="w-full h-auto rounded-lg" />
+          <div className="flex justify-center items-center align-middle mt-4">
+                      <h2 className="text-xl font-semibold">{data.productName}</h2><p className="font-bold text-xl ml-2">1kg</p>
+                  </div>
+          <p className="text-gray-700 mt-2">{data.description}</p>
+          <p className="text-lg font-bold mt-2">{formatPrice(data.prices)}</p>
+        </div>
+      </Link>
+      ))}
+      </div>
+  );
+};
+
+export default Product1kg;
